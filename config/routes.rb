9d3780e2134ba8f4d1e1d898724843a2d9b1684b
Casey Tansey <ctansey12600@outlook.com>
2022-11-07 13:38:05 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # POST /login: run the SessionsController#create method
+  post "/login", to: "sessions#create"
+
+  # GET /me: run the UsersController#show method
+  get "/me", to: "users#show"
+
+  # DELETE /logout: run the SessionsController#destroy method
+  delete "/logout", to: "sessions#destroy"
+
+
 end
